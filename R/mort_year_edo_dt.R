855c321c13eb3d1ff63e9aa7dbb462aa092f66db
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-09-09 16:21:09 -0500

    this commit update the function & add the column name clave in the table

--- R/mort_year_edo_dt.R
+++ R/mort_year_edo_dt.R
@@ -15,9 +15,9 @@ mort_year_edo_dt <- function(dataset, years, cve_edo, cat_group = NULL){
         x <- dataset |>
             dplyr::filter(ent_ocurr %in% c(cve_edo))|>
             dplyr::filter(anio_ocur %in% c(years))|>
-            dplyr::group_by(lista_mex_des, anio_ocur)|>
+            dplyr::group_by(lista_mex_des,lista_mex_cve, anio_ocur)|>
             dplyr::summarise(n = dplyr::n(), .groups = "drop") |>
-            tidyr::pivot_wider(id_cols = c(lista_mex_des),
+            tidyr::pivot_wider(id_cols = c(lista_mex_des, lista_mex_cve),
                                names_from = anio_ocur,
                                values_from = n,
                                names_sort = TRUE,
@@ -54,9 +54,9 @@ mort_year_edo_dt <- function(dataset, years, cve_edo, cat_group = NULL){
         dplyr::filter(ent_ocurr %in% c(cve_edo))|>
         dplyr::filter(anio_ocur %in% c(years))|>
         dplyr::filter(edad_des %in% c(vec)) |>
-        dplyr::group_by(lista_mex_des, anio_ocur)|>
+        dplyr::group_by(lista_mex_des, lista_mex_cve, anio_ocur)|>
         dplyr::summarise(n = dplyr::n(), .groups = "drop") |>
-        tidyr::pivot_wider(id_cols = c(lista_mex_des),
+        tidyr::pivot_wider(id_cols = c(lista_mex_des, lista_mex_cve),
                            names_from = anio_ocur,
                            values_from = n,
                            names_sort = TRUE,
@@ -68,7 +68,8 @@ mort_year_edo_dt <- function(dataset, years, cve_edo, cat_group = NULL){
         rowSums()
     x |>
         dplyr::arrange(dplyr::desc(total)) |>
-        dplyr::rename("Causa de Defuncion" = lista_mex_des)|>
+        dplyr::rename("Causa de Defuncion" = lista_mex_des,
+                      "Clave" = lista_mex_cve)|>
         kableExtra::kable() |>
         kableExtra::kable_styling() |>
         kableExtra:: scroll_box(width = "100%", height = "600px")
